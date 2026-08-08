422ea0280242b0d4c67f60c68cd01a01ba3fb642
User <evzhem@mail.ru>
2026-08-08 11:12:26 +0300

    Fix CI: permissions contents write для создания Release + versionName v7.0

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
         versionCode = 1
-        versionName = "v6.9.0"
+        versionName = "v7.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
